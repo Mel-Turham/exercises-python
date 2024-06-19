babffa02ade13398cc427b9b667c193b81459052
Mel/ <tchoikuemel06@gmail.com>
2024-06-19 01:35:37 +0100

    Add comments for each instructions in my code

--- 02-exo.py
+++ 02-exo.py
@@ -1,17 +1,38 @@
-#
+# This Python code is a simple authentication system. Here's a breakdown of what it does:
+
+# The lines `passWord = 'testpassword'` and `user_name = 'william'` are initializing variables in
+# Python.
 passWord = 'testpassword'
 user_name = 'william'
+# The lines `input_password = input('Please enter your password ... : ')` and `input_user_name =
+# input('Please enter your user name ... : ')` are prompting the user to enter their password and
+# username respectively. The `input()` function in Python is used to take user input from the console.
+# In this case, the user is being asked to input their password and username for authentication
+# purposes in the code.
 input_password = input('Please enter your password ... : ')
 input_user_name = input('Please enter your user name ... : ')
+# The line `message = print("Please enter the correct password and correct user name :( ")` is
+# assigning the result of the `print()` function to the variable `message`.
 message =  print("Please enter the correct password and correct user name :( ")
 
+# The `while` loop in the code snippet you provided is used for authentication purposes. It continues
+# to prompt the user to enter their password and username until both the entered password and username
+# match the predefined values (`passWord` and `user_name` respectively).
 while passWord  != input_password and user_name != input_user_name:
   input_password = input('Please enter your password ... : ')
   input_user_name = input('Please enter your user name ... : ')
   if(passWord != input_password or user_name != input_user_name):
     message
+# The code snippet you provided is checking if the entered password (`input_password`) matches the
+# predefined password (`passWord`) and if the entered username (`input_user_name`) matches the
+# predefined username (`user_name`).
     
 if passWord == input_password and input_user_name == user_name:
+   # The line `print(f"Welcome {user_name}, you logged successfully :)")` is responsible for
+   # displaying a welcome message to the user upon successful authentication. The `f` before the
+   # string indicates an f-string in Python, allowing for the interpolation of variables within the
+   # string. In this case, `{user_name}` will be replaced with the actual username of the
+   # authenticated user when the message is displayed.
     print(f"Welcome {user_name}, you logged successfully :)")
 
   

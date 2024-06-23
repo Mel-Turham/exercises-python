ce91b5ffefe30d6d69c76bfb7dd050b8029a4aed
Mel/ <tchoikuemel06@gmail.com>
2024-06-23 13:36:46 +0100

    some two lists

--- 12-exo.py
+++ 12-exo.py
@@ -1,8 +1,8 @@
-
 def somme_two_list(list_1, list_2):
   list_3 = []
-  
+
   for i in range(len(list_1)):
+
     if len(list_2) < len(list_1):
       print(f'Operation not possible ')
     else:
@@ -12,6 +12,7 @@ def somme_two_list(list_1, list_2):
   return list_3
 
 first_list = [1,2,3]
+
 second_list = [4,5,6]
 
 print(somme_two_list(first_list, second_list))
